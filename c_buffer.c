848b54621be0b8f6ea8104aa52161f1dcea60fed
Juan Olivares <jdop2000@gmail.com>
2020-04-09 17:16:48 +0000

    Refactoring code

---
+++ c_buffer.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+
+/**
+ * c_buffer - malloc create
+ *
+ * @st: getline
+ * @contador: lines contador
+ *
+ * Return: n characters per word
+ */
+void c_buffer(int contador, char *st) /*creation of malloc and send to exe*/
+{
+	char **space;
+	int i, j, ite = 0, letters = 0;
+	char *token;
+	char *ptr1;
+	char delim[] = " ";
+
+	space = malloc(sizeof(char*) * contador + 1);
+	if (space == NULL)
+	{
+		return;
+	}
+	ptr1 = strdup(st);
+	token = strtok(st, delim);
+
+	for (i = 0; i < contador || token != NULL; i++) /*contador = 1*/
+	{
+		letters = st_contador(token);
+		token = strtok(NULL, delim);
+		space[i] = malloc(sizeof(char) * letters);
+		if (space[i] == NULL)
+		{
+			for (ite = 0; ite < contador; ite++)
+			{
+				free(space[ite]);
+			}
+			free(space);
+			return;
+		}
+		for (j = 0; j < letters; j++)
+	      	{
+			space[i][j] = 0;
+		}
+	}
+	assignValues(space, ptr1, contador);
+	free(ptr1);
+}
--- shell.h
+++ shell.h
@@ -1,5 +1,7 @@
 #ifndef SHELL_H
 #define SHELL_H
+
+/* libraries */
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -8,9 +10,12 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+
+/* prototypes */
 void selector(char *s);
 void assignValues (char **ptr, char *s, int cont);
-int st_count (char *string);
+int st_contador (char *string);
 void exe(char **string);
 void c_buffer (int count, char *st);
+
 #endif /* SHELL_H */
