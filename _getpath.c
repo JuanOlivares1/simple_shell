5d6fce90e7a58982fe0572567e8d9d58346261c4
blsoko <exanymejose1@hotmail.com>
2020-04-15 22:58:06 +0000

    final review

---
+++ _getpath.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+/**
+ * _getpath - finds the values of a env-variable
+ *
+ * Return: value of env-variable. NULL if couldn't find
+ */
+
+char *_getpath(void)
+{
+	char buffer[1024];
+	char *path, *token;
+	char delim[] = {"\"="};
+	int fd, readLine, closeF;
+
+	fd = open("/etc/environment", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error:");
+		return (NULL);
+	}
+	for (readLine = 1024; readLine == 1024; )
+	{
+		readLine = read(fd, buffer, 1024);
+		if (readLine == -1)
+		{
+			perror("Error: ");
+			return (NULL);
+		}
+	}
+	token = strtok(buffer, delim);
+	if (token != NULL)
+	{
+		token = strtok(NULL, delim);
+		path = _strdup(token);
+	}
+	closeF = close(fd);
+	if (closeF == -1)
+	{
+		perror("Error: ");
+		return (NULL);
+	}
+	return (path);
+}
