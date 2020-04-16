8f1296dac5324aca6ac6b05acbef461b1d0dfdaa
Juan Olivares <jdop2000@gmail.com>
2020-04-16 19:28:05 +0000

    unset path

--- main.c
+++ main.c
@@ -12,13 +12,17 @@ int main(__attribute__((unused)) int ac, char **av)
 	char **grind;
 	char *buffer, *path;
 	size_t bufsize = 320;
-	int nargs, status = 0, line = 0, flag = 0;
+	int nargs, status = 0, line = 0, flag = 0, f = 0;
 
 	buffer = malloc(bufsize * sizeof(char));
 	if (buffer == NULL)
 		exit(1);
-	path = _strcat("/usr/local/sbin:/usr/local/bin:/usr/sbin:",
-		       "/usr/bin:/sbin:/bin:/usr/games:/usr/local/games");
+	path = _getenv("PATH");
+	if (path == NULL)
+	{
+		path = ".";
+		f = 1;
+	}
 	if (isatty(0) == 1)
 		write(1, "#cisfun$ ", 9);
 	while (getline(&buffer, &bufsize, stdin) != -1)
@@ -40,7 +44,8 @@ int main(__attribute__((unused)) int ac, char **av)
 	}
 	if (isatty(0) == 1 && flag == 0)
 		write(1, "\n", 1);
-	free(path);
+	if (f == 0)
+		free(path);
 	free(buffer);
 	return (status);
 }
